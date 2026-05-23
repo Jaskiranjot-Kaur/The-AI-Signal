cc9fc0416e11650fec8ff5c449b84cc628b8dac8
Jaskiranjot-Kaur <jaskiranjotkaur.tech@gmail.com>
2026-05-23 16:12:56 +0530

    Server Created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello! THE AI SIGNAL");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+// app.use("/api/websiteRouter");
+
+app.listen(port, () => {
+  console.log("Server started");
+});
